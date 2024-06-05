4e7d116427ce9d67c7e3da17e454e480115d76d7
Dirk Festerling <dfesterling@hotset.com>
2024-06-05 14:02:02 +0200

    added session ID and more configuration options for routing and logging

--- rabbitmq_message_publish/events.py
+++ rabbitmq_message_publish/events.py
@@ -10,27 +10,32 @@ def prepare_message_header (settings,user,event):
         language = frappe.session.data.lang
         if not language:
                 language = "en"
+
+        session = frappe.session.data.csrf_token
+        if not session:
+                session = "n.a."
                 
         return {
                 "transaction_id": str(uuid.uuid4()),
                 "timestamp": datetime.now(timezone.utc).isoformat(),
-                "session_id": "", #TODO: Web-Session auslesen? -> externen fragen
+                "session_id": session,
                 "operation": event,
                 "language": language,
-                "system": "erpnext", # TODO datapool
+                "system": settings.system,
                 "datapool": settings.datapool_name,
                 "hostname": socket.gethostname().upper(),
                 "user": user
         }
 
-def send_message (settings, message):
+def send_message (settings, exchange, message):
         try:
 
-                url = settings.url + "/api/exchanges/" +  settings.virtual_host + "/" + settings.message_exchange + "/publish"
+                url = settings.url + "/api/exchanges/" +  settings.virtual_host + "/" + exchange + "/publish"
 
                 response = requests.post(url, auth=(settings.username, settings.get_password("password")), json=message)
                 if response.status_code != 200:
-                        print(f"Failed to send log entry: {response.status_code} {response.text}")
+                        raise Exception (f"Failed to send log entry: {response.status_code} {response.text}")
+
         except Exception as err:
                 print(f'An unexpected error occurred: {err}')
 
@@ -43,6 +48,7 @@ def log_to_kibana(loglevel, message, offset = 0):
 
         settings = frappe.get_single('RabbitMQ Settings')
         if not settings.password:
+                # this is only logging - if RabbitMQ is not configured yet we silently ignore it
                 return
         
         username = frappe.session.user
@@ -64,11 +70,13 @@ def log_to_kibana(loglevel, message, offset = 0):
         body['payload'] = json.dumps(payload,ensure_ascii=False,indent=4,default=str)
         body['payload_encoding'] = "string"
 
-        send_message(settings,json.loads(json.dumps(body,ensure_ascii=False,indent=4)))
+        send_message(settings,"",json.loads(json.dumps(body,ensure_ascii=False,indent=4)))  
        
 
 def doctype_changed(doc, event):
         settings = frappe.get_single('RabbitMQ Settings')
+        if not settings.get_password:
+             raise Exception ("RabbitMQ Settings missing!") 
 
         global_id = doc.name
         if hasattr(doc,'global_id'):
@@ -82,7 +90,7 @@ def doctype_changed(doc, event):
 
         body = {}
         body['properties'] = {}
-        body['routing_key'] = f"erpnext.{settings.datapool_id}.{doc.doctype}"
+        body['routing_key'] = settings.system_id + "." + settings.datapool_id + "." + doc.doctype
 
         # json.dumps():
         #   ensure_ascii=false -> for formatting special characters correctly
@@ -92,4 +100,4 @@ def doctype_changed(doc, event):
         body['payload_encoding'] = "string"
         body_json = json.loads(json.dumps(body,ensure_ascii=False,indent=4))
 
-        send_message(settings,body_json)
+        send_message(settings,settings.message_exchange,body_json)
