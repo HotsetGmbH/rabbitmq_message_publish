02cf6f6cf25886f2661f677ef80c8d733488c462
Jannik Finke <jfinke@hotset.com>
2026-07-28 12:48:39 +0200

    HD-1351 added BOM doctype to hooks.py

--- rabbitmq_message_publish/hooks.py
+++ rabbitmq_message_publish/hooks.py
@@ -133,7 +133,7 @@ doc_events = {
 		"on_cancel":"rabbitmq_message_publish.events.doctype_changed",
 		"on_trash":"rabbitmq_message_publish.events.doctype_changed"
 	},
-	("Sales Order"):
+	("Sales Order","BOM"):
 	{
 		"on_update":"rabbitmq_message_publish.events.doctype_changed",
 		"on_submit":"rabbitmq_message_publish.events.doctype_changed",
